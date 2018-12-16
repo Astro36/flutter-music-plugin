87dfe193c577103bfa900b473854b59f84b39a9a
Seungjae Park <astro.psj@gmail.com>
2018-12-16 18:58:18 +0900

    Initial commit

---
+++ android/src/main/java/astro36/github/io/fluttermusicplugin/FlutterMusicPlugin.java
@@ -0,0 +1,25 @@
+package astro36.github.io.fluttermusicplugin;
+
+import io.flutter.plugin.common.MethodCall;
+import io.flutter.plugin.common.MethodChannel;
+import io.flutter.plugin.common.MethodChannel.MethodCallHandler;
+import io.flutter.plugin.common.MethodChannel.Result;
+import io.flutter.plugin.common.PluginRegistry.Registrar;
+
+/** FlutterMusicPlugin */
+public class FlutterMusicPlugin implements MethodCallHandler {
+  /** Plugin registration. */
+  public static void registerWith(Registrar registrar) {
+    final MethodChannel channel = new MethodChannel(registrar.messenger(), "flutter_music_plugin");
+    channel.setMethodCallHandler(new FlutterMusicPlugin());
+  }
+
+  @Override
+  public void onMethodCall(MethodCall call, Result result) {
+    if (call.method.equals("getPlatformVersion")) {
+      result.success("Android " + android.os.Build.VERSION.RELEASE);
+    } else {
+      result.notImplemented();
+    }
+  }
+}
