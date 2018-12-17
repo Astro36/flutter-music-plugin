6ef8f2968569126684bb0fc669f9d91013207d7c
Seungjae Park <astro.psj@gmail.com>
2018-12-17 22:39:14 +0900

    Update example app

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,7 +1,6 @@
-import 'package:flutter/material.dart';
-import 'dart:async';
+import 'dart:io';
 
-import 'package:flutter/services.dart';
+import 'package:flutter/material.dart';
 import 'package:flutter_music_plugin/flutter_music_plugin.dart';
 
 void main() => runApp(MyApp());
@@ -12,45 +11,82 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
-  String _platformVersion = 'Unknown';
+  MusicFinder _musicFinder = MusicFinder();
+  MusicPlayer _musicPlayer = MusicPlayer();
+
+  Widget _buildAlbumArt(Music music) {
+    if (music.albumArt != null) {
+      return CircleAvatar(backgroundImage: FileImage(File(music.albumArt)));
+    } else {
+      return CircleAvatar(child: Text(music.title[0]));
+    }
+  }
+
+  Widget _buildTitle(Music music) {
+    return Text(
+      music.title,
+      overflow: TextOverflow.ellipsis,
+    );
+  }
 
   @override
   void initState() {
     super.initState();
-    initPlatformState();
-  }
-
-  // Platform messages are asynchronous, so we initialize in an async method.
-  Future<void> initPlatformState() async {
-    String platformVersion;
-    // Platform messages may fail, so we use a try/catch PlatformException.
-    try {
-      platformVersion = await FlutterMusicPlugin.platformVersion;
-    } on PlatformException {
-      platformVersion = 'Failed to get platform version.';
-    }
-
-    // If the widget was removed from the tree while the asynchronous platform
-    // message was in flight, we want to discard the reply rather than calling
-    // setState to update our non-existent appearance.
-    if (!mounted) return;
-
-    setState(() {
-      _platformVersion = platformVersion;
-    });
   }
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text('Plugin example app'),
-        ),
-        body: Center(
-          child: Text('Running on: $_platformVersion\n'),
-        ),
-      ),
-    );
+        home: Scaffold(
+            appBar: AppBar(
+              title: const Text('Flutter Music Plugin Example App'),
+            ),
+            body: FutureBuilder(
+              future: _musicFinder.getAll(),
+              builder:
+                  (BuildContext context, AsyncSnapshot<List<Music>> snapshot) {
+                if (snapshot.connectionState == ConnectionState.done &&
+                    snapshot.hasData &&
+                    snapshot.data != null) {
+                  List<Music> musicList = snapshot.data;
+                  return ListView.builder(
+                      itemCount: musicList.length,
+                      itemBuilder: (BuildContext context, int index) {
+                        Music music = musicList[index];
+                        return ListTile(
+                            leading: _buildAlbumArt(music),
+                            title: _buildTitle(music),
+                            subtitle: Text(
+                                "${music.artist}\n${music.album} / ${music.genre}"),
+                            isThreeLine: true,
+                            onTap: () {
+                              _musicPlayer.prepare(music);
+                              _musicPlayer.start();
+                              showModalBottomSheet(
+                                  context: context,
+                                  builder: (BuildContext builder) {
+                                    return ListTile(
+                                      leading: _buildAlbumArt(music),
+                                      title: _buildTitle(music),
+                                      subtitle: Text(music.artist),
+                                      trailing: Icon(Icons.play_arrow),
+                                    );
+                                  }).then((value) => _musicPlayer.stop());
+                            });
+                      });
+                } else {
+                  return Center(
+                      child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: <Widget>[
+                      CircularProgressIndicator(),
+                      Padding(
+                          padding: EdgeInsets.only(top: 16),
+                          child: Text("Fetching music list...")),
+                    ],
+                  ));
+                }
+              },
+            )));
   }
 }
